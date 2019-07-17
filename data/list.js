5d7f0269ef1240555a5ceba3d5f17b7f3cadad7d
SmallPotatoCZ <sp.zhangchao@gmail.com>
2019-07-17 18:42:27 +0800

    add image

--- data/list.js
+++ data/list.js
@@ -2,6 +2,7 @@ var esl_list = {
 "images":{
 "3x2":[
 "315c721a-15a2-95f8-217e-6a3b9b84fcd3.jpg",
+"37abaffa-66ee-fba3-a099-9b9e7d7bc354.jpg",
 "5c7ecafd-bf18-b8f5-08b2-f3e5d39170a3.jpg",
 "61e4fa1b-6c53-b481-0604-f31919e8325b.jpg",
 ]
