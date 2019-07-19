c06a8f6851beed624c2bfba7918ab281a9dc235a
SmallPotatoCZ <sp.zhangchao@gmail.com>
2019-07-19 17:56:23 +0800

    rename small image

--- scripts/convertimg.sh
+++ scripts/convertimg.sh
@@ -6,7 +6,7 @@ for type in $( ls media ); do
     for size in $( ls media/$type ); do
         for file in $( ls media/$type/$size ); do
             if [[ -f media/$type/$size/$file ]]; then 
-                convert media/$type/$size/$file -resize 256x171 media/$type/$size/small/${file%.*}-small.${file##*.}
+                convert media/$type/$size/$file -resize 256x171 media/$type/$size/small/${file%.*}.${file##*.}
             fi
         done 
     done
